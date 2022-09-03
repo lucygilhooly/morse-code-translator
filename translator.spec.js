2fca13ab08350c9da5fa23d59d38864ae1a475d0
lucy gilhooly <lucy_johannah@hotmail.com>
2022-09-03 12:43:43 +0100

    TDD laid out

--- translator.spec.js
+++ translator.spec.js
@@ -1,7 +1,7 @@
 import { translateToMorse } from "./translator";
 
 describe('Testing translateToMorse', () => {
-  it("a should translate to be *-", ()=> {
+  it("'a' should translate to be *-", ()=> {
     // Arrange
       let string
     // Act
@@ -14,15 +14,58 @@ describe('Testing translateToMorse', () => {
  })
 
  describe('Testing translateToMorse', () => {
-    it("hello should translate to be .... . .-.. .-.. ---", () => {
+    it("'hello' should translate to be .... . .-.. .-.. ---", () => {
         let string = 'hello'
         let result = translateToMorse(string)
         expect(result).toBe('.... . .-.. .-.. ---');
     })
  })
- 
 
- //what should translateToMorse function actuall be able to do?
+ describe('Testing translateToMorse', () => {
+    it("' ' translate to be / ", () => {
+        let string = ' '
+        let result = translateToMorse(string)
+        expect(result).toBe('/');
+    })
+ })
+
+ describe('Testing translateToMorse', () => {
+    it("'hello i am lucy' should translate to be .... . .-.. .-.. --- / .. / .- -- / .-.. ..- -.-. -.--", () => {
+        let string = 'hello i am lucy'
+        let result = translateToMorse(string)
+        expect(result).toBe('.... . .-.. .-.. --- / .. / .- -- / .-.. ..- -.-. -.--');
+    })
+ })
+
+ describe('Testing translateToMorse', () => {
+    it("0 1 2 3 4 5 6 7 8 9 should translate to be ----- / .---- / ..--- / ...-- / ....- / ..... / -.... / --... / ---.. / ----.", () => {
+        let string = '0 1 2 3 4 5 6 7 8 9'
+        let result = translateToMorse(string)
+        expect(result).toBe('----- / .---- / ..--- / ...-- / ....- / ..... / -.... / --... / ---.. / ----.');
+    })
+ })
+
+ describe('Testing translateToMorse', () => {
+    it("'10' should translate to be .---- -----", () => {
+        let string = '10'
+        let result = translateToMorse(string)
+        expect(result).toBe('.---- -----');
+    })
+ })
+
+ describe('Testing translateToMorse', () => {
+    it(". , ? ; : - / '  should translate to be ._._._ / __..__ / ..__.. / _._._. / ___... / _...._ / _.._. / .____.", () => {
+        let string = ". , ? ; : - / '"
+        let result = translateToMorse(string)
+        expect(result).toBe('._._._ / __..__ / ..__.. / _._._. / ___... / _...._ / _.._. / .____.');
+    })
+ })
+
+
+
+
+
+ //what should translateToMorse function actually be able to do?
 
  // - recieve letters and translate those letters into morse code
  // - take into consideration captial letters - these also need to translate to morse code
