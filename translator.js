71ec3fb1e9904014aa6e4be886d702dd646d30f6
lucy gilhooly <lucy_johannah@hotmail.com>
2022-09-03 13:05:01 +0100

    called all necessary elements from HTML

--- translator.js
+++ translator.js
@@ -1,3 +1,13 @@
+const inputBox = document.querySelector("#text-input");
+const translateBtn = document.querySelector("#translate-btn");
+
+// 
+
+
+// need array of objects for the alphabet, numbers, special symbols
+// alphabet 
+
+
 export const translateToMorse = () => {
-    
+
 }
