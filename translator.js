d15525c89a1ed9eeebc75533d741e4e7fc33bb36
lucy gilhooly <lucy_johannah@hotmail.com>
2022-09-04 16:11:42 +0100

    changed plan complately in js

--- translator.js
+++ translator.js
@@ -1,64 +1,22 @@
-const inputBox = document.querySelector("#text-input");
-const translateBtn = document.querySelector("#translate-btn");
-const output = document.querySelector("#output");
+// instead of grabbing everything at the top of the page, called document.getElementById within the function
 
-// need object for the alphabet, numbers, special symbols
-const morseCode = {
-    "A": ".-",
-    "B": "-...",
-    "C": "-.-.",
-    "D": "-..",
-    "E": ".",
-    "F": "..-.",
-    "G": "--.",
-    "H": "....",
-    "I": "..",
-    "J": ".---",
-    "K": "-.-",
-    "L": ".-..",
-    "M": "--",
-    "N": "-.",
-    "O": "---",
-    "P": ".--.",
-    "Q": "--.-",
-    "R": ".-.",
-    "S": "...",
-    "T": "-",
-    "U": "..-",
-    "W": ".--",
-    "X": "-..-",
-    "Y": "-.--",
-    "Z": "--..",
-    " ": "/",
-    "0": "-----",
-    "1": ".----",
-    "2": "..---",
-    "3": "...--",
-    "4": "....-",
-    "5": ".....",
-    "6": "-....",
-    "7": "--...",
-    "8": "---..",
-    "9": "----.",
-    ".": "._._._",
-    ",": "__..__",
-    "?": "..__..",
-    ";": "_._._.",
-    ":": "___...",
-    "-": "_...._",
-    "/": "_.._.",
-    "'": ".____."
+function translateToMorse(plainText) {
+    // set the text value to an empty string
+    document.getElementById("myText").value = "";
+    //store morse code and the alphabet within arrays to allow us to iterate through them with a for loop
+    const morseCode = [".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--..", ".----", "..---", "...--", "....-", "....", "-....", "--...", "---..", "----.", "-----", "/", "._._._", "__..__", "..__..", "_._._.", "___...", "_...._", "_.._.", ".____."];
+    const alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", " ", ".", ",", "?", ";", ":", "-", "/", "'"];
+    var text = "";
+    //for loop going through the inputted text 
+    for (var i = 0; i < plainText.length; i++) {
+        for (var j = 0; j < alphabet.length; j++) {
+            // converting inputted text to caps to compare to alphabet array
+            
+            if (plainText[i].toUpperCase() == alphabet[j]) {
+                text += morseCode[j] + " ";
+            }
+        }
+    }
 
- }
-
-
-export const translateToMorse = () => {
-
+    document.getElementById("morseCode").innerHTML = text;
 }
-
-// output
-// inputBox.value.innerHTML = output
-// however must be declared within function after the translation has occurred 
-
-// event listener for the button to run the function
-translateBtn.addEventListener("click", translateToMorse);
