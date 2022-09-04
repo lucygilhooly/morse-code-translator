160b89cd1fcd2c9464ad009a462119593c2bcbcf
lucy gilhooly <lucy_johannah@hotmail.com>
2022-09-04 16:12:39 +0100

    translator functions

---
+++ dist/translator.dev.js
@@ -0,0 +1,22 @@
+"use strict";
+
+// instead of grabbing everything at the top of the page, called document.getElementById within the function
+function translateToMorse(plainText) {
+  // set the text value to an empty string
+  document.getElementById("myText").value = ""; //store morse code and the alphabet within arrays to allow us to iterate through them with a for loop
+
+  var morseCode = [".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--..", ".----", "..---", "...--", "....-", "....", "-....", "--...", "---..", "----.", "-----", "/", "._._._", "__..__", "..__..", "_._._.", "___...", "_...._", "_.._.", ".____."];
+  var alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", " ", ".", ",", "?", ";", ":", "-", "/", "'"];
+  var text = ""; //for loop going through the inputted text 
+
+  for (var i = 0; i < plainText.length; i++) {
+    for (var j = 0; j < alphabet.length; j++) {
+      // converting inputted text to caps to compare to alphabet array
+      if (plainText[i].toUpperCase() == alphabet[j]) {
+        text += morseCode[j] + " ";
+      }
+    }
+  }
+
+  document.getElementById("morseCode").innerHTML = text;
+}
