bc44d765dcaea268ba23a41a1ee99f9e4871dd28
lucy gilhooly <lucy_johannah@hotmail.com>
2022-09-05 14:17:21 +0100

    fixed spelling mistakes

--- dist/translator.dev.js
+++ dist/translator.dev.js
@@ -4,19 +4,16 @@ Object.defineProperty(exports, "__esModule", {
   value: true
 });
 exports.translateToMorse = void 0;
-//store morse code and the alphabet within arrays to allow us to iterate through them with a for loop 
-var morseCode = [".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--..", ".----", "..---", "...--", "....-", "....", "-....", "--...", "---..", "----.", "-----", "/", "._._._", "__..__", "..__..", "_._._.", "___...", "_...._", "_.._.", ".____."];
+var morseCode = [".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--..", ".----", "..---", "...--", "....-", ".....", "-....", "--...", "---..", "----.", "-----", "/", "._._._", "__..__", "..__..", "_._._.", "___...", "_...._", "_.._.", ".____."];
 var alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", " ", ".", ",", "?", ";", ":", "-", "/", "'"];
 
 var translateToMorse = function translateToMorse(plainText) {
-  //for loop going through the inputted text 
   var result = [];
 
   for (var i = 0; i < plainText.length; i++) {
     for (var j = 0; j < alphabet.length; j++) {
-      // converting inputted text to caps to compare to alphabet array
       if (plainText[i].toUpperCase() == alphabet[j]) {
-        result.push(morseCode[j] + " ");
+        result.push(morseCode[j]);
       }
     }
   }
--- main.js
+++ main.js
@@ -1,8 +1,9 @@
-import {translateToMorse} from "./translator.js"
+import {
+    translateToMorse
+} from "./translator.js"
 
-document.getElementById("myText").addEventListener("change",() => {
-    let input = document.getElementById("myText").value
-   const translation = translateToMorse(input)
-   document.getElementById("morseCode").innerHTML = translation;
+document.getElementById("myText").addEventListener("change", () => {
+    let input = document.getElementById("myText").value;
+    const translation = translateToMorse(input);
+    document.getElementById("morseCode").innerHTML = translation;
 })
-
